6207666d8a70e8deae5e0c3bc15a1b6a2c01a337
Dave Webb <wfh@dcw.co.nz>
2015-09-21 13:44:24 +1200

    Initial commit for Pod::Markdown::Passthrough
    Repo created with:minil new Pod::Markdown::Passthrough

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Pod::Markdown::Passthrough - It's new $module
+
+# SYNOPSIS
+
+    use Pod::Markdown::Passthrough;
+
+# DESCRIPTION
+
+Pod::Markdown::Passthrough is ...
+
+# LICENSE
+
+Copyright (C) Dave Webb.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Dave Webb <wfh@dcw.co.nz>
---
+++ lib/Pod/Markdown/Passthrough.pm
@@ -0,0 +1,39 @@
+package Pod::Markdown::Passthrough;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Pod::Markdown::Passthrough - It's new $module
+
+=head1 SYNOPSIS
+
+    use Pod::Markdown::Passthrough;
+
+=head1 DESCRIPTION
+
+Pod::Markdown::Passthrough is ...
+
+=head1 LICENSE
+
+Copyright (C) Dave Webb.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Dave Webb E<lt>wfh@dcw.co.nzE<gt>
+
+=cut
+
